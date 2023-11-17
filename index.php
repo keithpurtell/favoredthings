46716b9a92c669a423a6d781ed626a745b7a3632
keithpurtell <51518518+keithpurtell@users.noreply.github.com>
2023-11-17 13:27:40 -0600

    Add files via upload
    Just the index file, for now.

---
+++ index.php
@@ -0,0 +1,191 @@
+<!doctype html>
+<html lang="en" prefix="og: https://ogp.me/ns#">
+<head>
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<meta charset="utf-8">
+<title>These Favored Things (not an online portfolio)</title>
+<meta name="description" content="In North America's media-driven rush of events and tendency to blindly follow the perceived mainstream, we miss a lot.">
+<meta http-equiv="Content-Security-Policy" content="default-src 'none'; script-src 'self' 'unsafe-inline' https://keithpurtell.com/js/ https://www.clarity.ms https://www.google-analytics.com https://www.gstatic.com https://www.google.com nonce-36e95bf5d242af8226c17f1c5ae41241d626880e4fcb nonce-ec0caa3e1f3a8f4ec1935679027c1f361422efe00150; style-src 'self' 'unsafe-inline'; object-src 'none'; base-uri 'self'; connect-src 'self'; font-src 'self' https://fonts.gstatic.com; frame-src 'self' https://www.google.com https://www.youtube.com; img-src 'self'  https://www.paypalobjects.com https://c.clarity.ms; manifest-src 'self'; media-src 'self'; worker-src 'none';">
+<script type="application/ld+json" nonce="36e95bf5d242af8226c17f1c5ae41241d626880e4fcb">
+{
+  "@context": "https://schema.org/",
+  "@type": "Article",
+  "mainEntityOfPage": {
+    "@type": "WebPage",
+    "@id": "https://keithpurtell.com/index.php"
+  },
+  "author": {
+    "@type": "Person",
+    "name": "Keith Purtell"
+  },
+  "publisher": {
+    "@type": "Organization",
+    "name": "These Favored Things",
+    "logo": {
+      "@type": "ImageObject",
+      "url": "https://keithpurtell.com/images/for-jsonld.jpg"
+    }
+  },
+  "headline": "Overlooked. Underestimated.",
+  "image": "https://keithpurtell.com/images/keith-purtell-com-paving-stones.webp",
+  "datePublished": "1998-04-15",
+  "dateModified": "2023-09-12"
+}
+</script>
+<?php include_once('js/clarity.js'); ?>
+<meta name="robots" content="index, follow">
+<link rel="manifest" href="/manifest.json">
+<meta name="dcterms.rightsHolder" content="Copyright © 2021 Keith Purtell. All Rights Reserved. NOT an online portfolio.">
+<link rel="canonical" href="https://keithpurtell.com/index.php">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="theme-color" content="#fff3e3">
+<link rel="stylesheet" type="text/css" href="css/stylemain.css" media="screen">
+<link rel="stylesheet" href="css/print.css" media="print">
+<link rel="icon" type="image/vnd.microsoft.icon" href="favicon.ico">
+<meta property="og:title" content="These Favored Things. Overlooked. Underestimated.">
+<meta property="og:description" content="Cultural features that either haven't received the attention they deserve or merit a second look. NOT an online portfolio.">
+<meta property="og:image" content="https://keithpurtell.com/images/keith-purtell-com-paving-stones.webp">
+<meta property="og:url" content="https://keithpurtell.com/index.php">
+<meta property="og:type" content="website">
+<meta property="fb:app_id" content="947558702098836">
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:site" content="@keithpurtell">
+<meta name="twitter:title" content="These Favored Things. Overlooked. Underestimated.">
+<meta name="twitter:description" content="Cultural features that either haven't received the attention they deserve or merit a second look. NOT an online portfolio.">
+<meta name="twitter:creator" content="@keithpurtell">
+<meta name="twitter:image" content="https://keithpurtell.com/images/keith-purtell-com-paving-stones.webp">
+<link rel="icon" type="image/vnd.microsoft.icon" href="https://keithpurtell.com/favicon.ico">
+<meta name="application-name" content="these favored things">
+<meta name="msapplication-TileColor" content="#996600">
+<meta name="theme-color" content="#996600">
+<meta name="msapplication-TileColor" content="#996600">
+<style>
+li#pform * {
+margin:0;
+padding:0;
+display:block;
+}
+html body div.wrapper div#main div#navigate ul#navlist li#pform a form.paypal input#donate, html body div.wrapper div#navigate ul#navlist li#pform a form.paypal input#donate {
+height:26px !important;
+width:92px;
+}
+/* reset is above */
+input#donate {
+border:none;
+background-color:transparent;}
+#article > p:nth-child(5) > img {float:right;width:auto;}
+ul#navlist > li#pform {
+padding-top: 0.2em !important;/* override main stylesheet for this pg */
+}
+ul#navlist > li#pform > a {
+padding:0 !important;/* override main stylesheet for this pg */
+}
+.rgt {
+float:right;
+margin:0 1em;
+}
+.holdsimg {
+padding:0;
+}
+#main .img_wid_col_spl {
+margin:0;
+padding:0;
+width:100%;
+}
+img:not(#pform > a > form > img) {
+  border:2px solid #333333; /* MAY TAKE THIS SITE WIDE ================== */
+}
+
+@media only screen and (min-width: 320px) and (max-width: 767px) /* phones */ {}
+
+@media only screen and (min-width: 992px) /* DESKTOPS */ {
+#main > .hdsho {
+margin-right:90px;
+} }
+</style>
+<script nonce="ec0caa3e1f3a8f4ec1935679027c1f361422efe00150">function toggleNav() {document.getElementById("navlist").classList.toggle("open");}</script>
+</head>
+
+<body class="contact">
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId: '947558702098836',
+      xfbml: true,
+      version: 'v18.0'
+    });
+  };
+</script>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js"></script>
+<a href="#main" class="visible-on-focus">Skip to main content</a>
+<?php include_once("analyticstracking.php") ?>
+<div id="header"><p>THESE FAVORED THINGS</p></div>
+<div class="lbox lboxb"></div>
+<div class="hamburger" onclick="toggleNav()"></div>
+<div class="wrapper">
+<div id="navigate">
+<ul id="navlist">
+<li><a href="index.php">Home</a></li>
+<li><a href="bj-cole.php">Music</a></li>
+<li><a href="john-greenway.php">Authors</a></li>
+<li><a href="Powwow-brings-Native-Americans-together.php">Ideas</a></li>
+<li><a href="eugene-atget.php">Design</a></li>
+<li><a href="eclecticism.php">Other</a></li>
+<li id="pform"><a href="#" rel="noopener">
+<form target="_blank" action="https://www.paypal.com/cgi-bin/webscr" method="post" class="paypal">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="2ZAZ897G65KF4">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" id="donate" name="submit" alt="Donate to Keith Purtell / These Favored Things">
+<img loading="lazy" alt="pixel-paypal" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+</a></li>
+</ul>
+</div><!-- end div navigate -->
+<article id="main" tabindex="-1">
+
+<h1>Overlooked. Underestimated.</h1>
+<h2>Consider These...</h2>
+<h3 class="author">by Keith Purtell</h3>
+<img src="images/Keith_Purtell_2012-150.webp" width="150" height="185" class="rgt hdsho" alt="Keith Purtell in 2012"><p>Cultural features that either haven&#8217;t received the attention they deserve or merit a second look. In North America&#8217;s media-driven rush, and our tendency to blindly follow a perceived mainstream, we miss a lot. I hope you will be inspired to seek these out.</p>
+<p><strong>Not a portfolio:</strong> The site was conceived as a way of being both web developer and content provider. Plus, it was fun. I rarely add new pages. Mostly I use this site to tinker. The bulk of my professional attention goes to current clients.</p>
+<p class="holdsimg"><img src="images/keith-purtell-com-paving-stones.webp" width="762" height="400" class="img_wid_col_spl" alt="These Favored Things. Overlooked. Underestimated."></p>
+
+<p>The earliest captured version of keithpurtell.com ("Favored Things") is May 30, 2000 at the Internet Archive. The site actually started as a 1998 user account at an Overland Park, Kansas, ISP called Unicom.net. The Oct. 15, 2000 version is still visible as <a href="https://web.archive.org/web/20001015231852/https://keithpurtell.com/" class="nlnk" target="_blank" rel="noopener">TFT at the Wayback Machine</a></p>
+<!-- https://web.archive.org/web/20001015231852/https://keithpurtell.com/
+https://web.archive.org/web/*/https://keithpurtell.com/
+  -->
+
+<p>In that 1998/2000 version of Favored Things, I included an article on the <a href="https://web.archive.org/web/20021115033802fw_/https://www.keithpurtell.com/kthings/frmst_riot.htm" class="nlnk" target="_blank" rel="noopener">Tulsa Race Riot (Tulsa Race Massacre)</a>, which I had heard about in the late 1980s.</p>
+
+<h4>Privacy Policy</h4>
+<p>See this site's <a href="https://keithpurtell.com/privacy.php" target="_blank" rel="noopener">privacy policy at this link</a> (new tab/window)</p>
+
+<h4>Lawsuits</h4>
+<p>Contacting me is a much more efficient and honest way of correcting errors than bringing a lawsuit against the author(s). Bringing a lawsuit instead of asking for a correction reveals the underlying motives of the person or persons who have chosen to abuse the legal system to intimidate citizen journalists.</p>
+</article><!-- end article id=main -->
+</article><!-- end article main -->
+<p>&nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;</p>
+</div><!-- END wrapper  -->
+
+<div id="binder"><!-- binds color theme bars -->
+<div class="bbox bboxb"></div>
+<div id="ftr"></div>
+</div><!-- end binder  -->
+<script src="https://www.google.com/recaptcha/api.js?render=6LeFf68iAAAAAHuRovTxOwssYcixXJFR6Wh27LAq"></script>
+<script>
+      function onClick(e) {
+        e.preventDefault();
+        grecaptcha.ready(function() {
+          grecaptcha.execute('6LeFf68iAAAAAHuRovTxOwssYcixXJFR6Wh27LAq', {action: 'submit'}).then(function(token) {
+              // Add your logic to submit to your backend server here.
+          });
+        });
+      }
+  </script>
+</body>
+</html>
+<!-- 
+
+meta http-equiv="Content-Security-Policy" content="default-src 'none'; frame-ancestors 'none'; font-src 'self' https://fonts.gstatic.com; frame-src https://www.google.com https://www.youtube.com; img-src 'self' https://www.paypalobjects.com; media-src 'self'" 
+
+ -->
